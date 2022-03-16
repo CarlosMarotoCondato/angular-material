a1e4ecaaaaa1bf9b1efb8a756c490fe5c639c067
CarlosMarotoCondato <carlos.maroto.condato@gmail.com>
2022-03-16 09:10:40 +0100

    Terminado proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { FormComponent } from './components/form/form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inicio', component: InicioComponent},
+  {path: 'formulario', component: FormComponent},
+  {path: '', redirectTo: "inicio", pathMatch: "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatBadgeModule } from '@angular/material/badge';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { CommonModule } from '@angular/common';
+import {MatPaginatorModule} from '@angular/material/paginator';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { CardsComponent } from './components/cards/cards.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { FormComponent } from './components/form/form.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardsComponent
+    CardsComponent,
+    InicioComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
@@ -20,7 +33,16 @@ import { CardsComponent } from './components/cards/cards.component';
     MatToolbarModule,
     MatCardModule,
     MatButtonModule,
-    MatGridListModule
+    MatGridListModule,
+    MatMenuModule,
+    BrowserAnimationsModule,
+    MatBadgeModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    CommonModule,
+    MatPaginatorModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
